66f26cdc117da03714a7a47d3d9c786593a99960
ryanpham <ryanphamit@gmail.com>
2024-06-07 16:15:57 +0700

    refactory code #1

---
+++ src/js/model.js
@@ -0,0 +1,38 @@
+import 'regenerator-runtime';
+
+export const state = {
+  recipe: {},
+  search: {
+    query: '',
+    results: [],
+    page: 1,
+    resultsPerPage: 10,
+  },
+  bookmarks: [],
+};
+
+export const loadRecipe = async function (id) {
+  try {
+    const res = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
+    );
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+    const { recipe } = data.data;
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+  } catch (err) {
+    console.error(`${err} 🔥🔥🔥`);
+    throw err;
+  }
+};
